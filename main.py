85f019840d3923d168c5a8e0faddc63bb1897911
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2024-03-31 16:02:39 +0900

    경로 매개변수와 숫자 검증

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-from typing import Union
-
-from fastapi import FastAPI, Query
-
 from pydantic.json_schema import SkipJsonSchema
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 
-@app.get("/items/")
+@app.get("/items/{item_id}")
 async def read_items(
-    q: Union[str, SkipJsonSchema[None]] = Query(
-        default=None,
-        title="Query string",
-        description="Query string for the items to search in the database that have a good match",
-        min_length=3,
-        deprecated=True,
-    ),
+    *,
+    item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+    q: str,
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
-
-# @app.get("/items/")
-# async def read_items(q: Union[str, SkipJsonSchema[None]] = Query(default=None, alias="item-query")):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     if q:
-#         results.update({"q": q})
-#     return results
