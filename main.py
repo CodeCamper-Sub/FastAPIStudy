5288980baad83b1d8e85b0c4ca67e79c7de39dc0
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2024-03-31 15:16:34 +0900

    쿼리 매개변수와 문자열 검증

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Union
 
+from fastapi import FastAPI, Query
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from pydantic.json_schema import SkipJsonSchema
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/")
+async def read_items(
+    q: Union[str, SkipJsonSchema[None]] = Query(
+        default=None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        deprecated=True,
+    ),
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
+
+# @app.get("/items/")
+# async def read_items(q: Union[str, SkipJsonSchema[None]] = Query(default=None, alias="item-query")):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
