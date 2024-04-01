a6e4be0d3bae052bcd3598e6b32e27fce007df24
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2024-04-01 20:36:17 +0900

    본문 - 다중 매개변수

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from pydantic.json_schema import SkipJsonSchema
-from fastapi import FastAPI, Path
+from typing import Union
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    *,
-    item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
-    q: str,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+class Item(BaseModel):
+    name: str
+    description: Union[str, SkipJsonSchema[None]] = None
+    price: float
+    tax: Union[float, SkipJsonSchema[None]] = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, SkipJsonSchema[None]] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     return results
