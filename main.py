91c3ca331e8bda304092dcb7a2b44b4433f7c5c0
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2024-04-01 20:51:26 +0900

    본문 - 중첩 모델

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from pydantic.json_schema import SkipJsonSchema
-from typing import Annotated
+# SkipJsonSchema[None]
+from typing import List, Set, Union, Dict
 
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
-    description: str | SkipJsonSchema[None] = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
-    tax: float | SkipJsonSchema[None] = None
+    description: Union[str, SkipJsonSchema[None]] = None
+    price: float
+    tax: Union[float, SkipJsonSchema[None]] = None
+    tags: Set[str] = set()
+    images: Union[List[Image], SkipJsonSchema[None]] = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
-    return results
+class Offer(BaseModel):
+    name: str
+    description: Union[str, SkipJsonSchema[None]] = None
+    price: float
+    items: List[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer = Body(embed=True)):
+    return offer
+
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
+
