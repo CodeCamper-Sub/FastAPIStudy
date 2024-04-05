261fbcf14cb5d5eabacbf8015ed8ba1c31000bd8
Eric/한경수 <gksrudtn99@naver.com>
2024-04-05 14:41:21 +0900

    헤더 매개변수

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pydantic.json_schema import SkipJsonSchema
 # SkipJsonSchema[None]
-from typing import Annotated
+from typing import List, Union
 
-from fastapi import Cookie, FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(ads_id: Annotated[str | SkipJsonSchema[None], Cookie()] = None):
-    return {"ads_id": ads_id}
+async def read_items(x_token: Union[List[str], SkipJsonSchema[None]] = Header(default=None)):
+    return {"X-Token values": x_token}
