61d9293c12ef95b60c3f1f56f508c3c548079e0e
Eric/한경수 <gksrudtn99@naver.com>
2024-04-08 14:40:59 +0900

    의존성

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from pydantic.json_schema import SkipJsonSchema
+# SkipJsonSchema[None]
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str | None = None
-    description: str | None = None
-    price: float | None = None
-    tax: float = 10.5
-    tags: list[str] = []
+async def common_parameters(q: str | SkipJsonSchema[None] = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+CommonsDep = Annotated[dict, Depends(common_parameters)]
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
+@app.get("/items/")
+async def read_items(commons: CommonsDep):
+    return commons
 
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: str):
-    return items[item_id]
-
-
-@app.put("/items/{item_id}", response_model=Item)
-async def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
-
-@app.patch("/items/{item_id}", response_model=Item)
-async def patch_item(item_id: str, item: Item):
-    stored_item_data = items[item_id]
-    stored_item_model = Item(**stored_item_data)
-    update_data = item.model_dump(exclude_unset=True)
-    updated_item = stored_item_model.model_copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
+@app.get("/users/")
+async def read_users(commons: CommonsDep):
+    return commons
