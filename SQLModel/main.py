40efd56588cbc290d92f4be69bbc6edd29188947
Eric/한경수 <gksrudtn99@naver.com>
2024-04-09 13:23:53 +0900

    SQLModel: Create a Table with SQLModel - Use the Engine - 2

--- SQLModel/main.py
+++ SQLModel/main.py
@@ -1,7 +1,23 @@
-from database import engine, SQLModel
+from sqlmodel import Field, SQLModel, create_engine  
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":
+class Hero(SQLModel, table=True):  
+    id: int | None = Field(default=None, primary_key=True)  
+    name: str  
+    secret_name: str  
+    age: int | None = None  
+
+
+sqlite_file_name = "database.db"  
+sqlite_url = f"sqlite:///{sqlite_file_name}"  
+
+engine = create_engine(sqlite_url, echo=True)  
+
+
+def create_db_and_tables():  
+    SQLModel.metadata.create_all(engine)  
+
+
+if __name__ == "__main__":  
     create_db_and_tables()
+    
