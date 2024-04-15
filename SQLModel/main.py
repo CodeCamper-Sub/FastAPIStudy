c1fef3fb184f52f2cc94f266e845bf70c76f4507
Eric/한경수 <gksrudtn99@gmail.com>
2024-04-15 11:10:25 +0900

    Create Connected Tables

--- SQLModel/main.py
+++ SQLModel/main.py
@@ -1,11 +1,19 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
+class Team(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
+
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
+
+    team_id: int | None = Field(default=None, foreign_key="team.id")
 
 
 sqlite_file_name = "database.db"
